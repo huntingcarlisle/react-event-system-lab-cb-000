390fadee14c262b52069a0e1b0bf3c25f6d64927
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-27 04:40:10 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 export default class Keypad extends React.Component {
   render() {
     return (
-      <input onKeyUp={console.log('Entering password...'} type='password' />
+      <input onKeyUp={console.log('Entering password...')} type='password' />
       )
   }
 }
