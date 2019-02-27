58f127bc209e2778d40b24e9f60cae9fc01d7d7d
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-27 04:37:32 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class EyesOnMe extends React.Component {
+  render() {
+    return (
+      <input type='password' />
+      )
+  }
+}
